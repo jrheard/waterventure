0a87e5ba0741e8fe48570e7a2c0fc1b81845fab6
jrheard <jrheard@cs.stanford.edu>
2017-10-06 13:23:28 -0700

    WIP

--- waterventure.py
+++ waterventure.py
@@ -3,10 +3,10 @@ from collections import namedtuple
 from madison_wcb import wcb
 
 
-Room = namedtuple("Room", ["x", "y", "width", "height", "color_index", "exits", "description"])
+Room = namedtuple("Room", ["id", "x", "y", "width", "height", "color_index", "exits", "description"])
 
-end_room = Room(100, 100, 100, 100, 2, {}, "There is a wizard here. He knights you.")
-start_room = Room(-200, -200, 100, 100, 0, {'east': end_room}, "It is dark, you might be eaten by a grue.")
+end_room = Room(0, 100, 100, 100, 100, 2, {}, "There is a wizard here. He knights you.")
+start_room = Room(1, -200, -200, 100, 100, 0, {'east': end_room}, "It is dark, you might be eaten by a grue.")
 
 state = {
     'current_room': start_room,
@@ -28,10 +28,24 @@ def process_command(command):
         print("I don't know how to do that. Try something like 'go east'.")
 
 def render_room(room):
-    if room not in state['drawn_rooms']:
-        state['drawn_rooms'].add(room)
+    if room.id not in state['drawn_rooms']:
+        state['drawn_rooms'].add(room.id)
 
+        wcb.get_color(room.color_index)
+        wcb.move_to(room.x, room.y)
+        wcb.brush_down()
 
+        wcb.point_in_direction(0)
+        wcb.move_forward(room.width)
+        wcb.turn_right(90)
+        wcb.move_forward(room.height)
+        wcb.turn_right(90)
+        wcb.move_forward(room.width)
+        wcb.turn_right(90)
+        wcb.move_forward(room.height)
+        wcb.brush_up()
+
+    wcb.move_to(room.x + room.width / 2, room.y + room.height / 2)
 
     print(room.description)
     if room.exits:
